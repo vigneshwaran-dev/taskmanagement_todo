8aa32fadebef1be2a643ebb6d5b8d5a0b5b145d2
Vigneshwaran S <58353559+vigneshwaran-dev@users.noreply.github.com>
2020-06-01 19:45:18 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import VCalendar from 'v-calendar';
 Vue.config.productionTip = false;
 Vue.use(VCalendar);
 
-//Set the server API address here
-Vue.prototype.$API = 'https://todoserver-hackerearth.herokuapp.com';
+//Set the server API address here and the address should not end with a /(slash)
+Vue.prototype.$API = '';
 
 new Vue({
   store,
